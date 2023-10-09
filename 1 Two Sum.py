9b0fb8ee0b0cd5b4d9892278df88c25406a037cb
Lvpeipei <peihenglyu@gmail.com>
2023-10-08 23:56:11 -0500

    1 Two Sum.py

---
+++ 1 Two Sum.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        
+        for i in range(len(nums)):
+            for j in range(len(nums)):
+                if nums[i] + nums[j] == target and i != j:
+                    return [i,j]
+        
+        return False
+    
+obj = Solution()
+nums = [0,4,3,0]
+print(obj.twoSum(nums,0))
