16d1b28de64b0ed7399d7a308f4d3a0c2a0329f4
Lvpeipei <peihenglyu@gmail.com>
2023-10-21 22:09:41 -0500

    98 Validate Binary Search Tree.py (final)

--- 98 Validate Binary Search Tree.py
+++ 98 Validate Binary Search Tree.py
@@ -5,20 +5,49 @@ class TreeNode(object):
         self.left = left
         self.right = right
 
+# # My solution
+# class Solution(object):
+#     def isValidBST(self, root):
+#         """
+#         :type root: TreeNode
+#         :rtype: bool
+#         """
+#         def helper(root):
+#             if not root:
+#                 return (True, float('inf'), float('-inf'))
+            
+#             test_left = True
+#             test_right = True
+
+#             left = helper(root.left)
+#             right = helper(root.right)
+
+#             if root.val <= left[2]:
+#                 test_left =  False
+
+#             if root.val >= right[1]:
+#                 test_right = False
+
+#             return ( (test_left and test_right and left[0] and right[0]), min(root.val, left[1]), max(root.val, right[2]) )
+#         return helper(root)[0]
+
+# Better solution
 class Solution(object):
     def isValidBST(self, root):
         """
         :type root: TreeNode
         :rtype: bool
         """
-        if not root:
-            return True
-        test_left = True
-        test_right = True
-        if root.left:
-            if root.left.val >= root.val:
-                test_left = False
-        if root.right:
-            if root.right.val <= root.val:
-                test_right = False
-        return self.isValidBST(root.left) and self.isValidBST(root.right) and test_left and test_right
+
+        def dfs(node, lower=float('-inf'), upper=float('inf')):
+            if not node:
+                return True 
+            
+            val=node.val
+            if val<=lower or val>=upper:
+                return False 
+
+            return dfs(node.left,lower,val) and dfs(node.right,val, upper)
+            
+        
+        return dfs(root)
