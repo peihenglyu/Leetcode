a6b0d9a2df56e831b1bf6c65924a1eb8bb6483a3
Lvpeipei <peihenglyu@gmail.com>
2024-01-04 16:17:17 -0600

    9 Palindrome Number.py

---
+++ 9 Palindrome Number.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        x_str = str(x)
+        left = 0
+        right = len(x_str) - 1
+        while left <= right:
+            if x_str[left] != x_str[right]:
+                return False
+            left += 1
+            right -= 1
+
+        return True
+
+obj = Solution()
+print(obj.isPalindrome(121))
