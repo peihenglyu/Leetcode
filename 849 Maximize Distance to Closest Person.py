29e79c3f128d2029054777a0461eae2ddcaa8551
Lvpeipei <peihenglyu@gmail.com>
2024-02-22 18:00:01 -0600

    849 Maximize Distance to Closest Person.py

---
+++ 849 Maximize Distance to Closest Person.py
@@ -0,0 +1,47 @@
+class Solution(object):
+    def maxDistToClosest(self, seats):
+        """
+        :type seats: List[int]
+        :rtype: int
+        """
+        index = 0
+        left_zero = 0
+        right_zero = 0
+        while seats[index] != 1 and index < len(seats):
+            left_zero += 1
+            index += 1
+        index = len(seats)-1
+        while seats[index] != 1 and index < len(seats):
+            right_zero += 1
+            index -= 1
+        
+        if left_zero:
+            left_zero_list = [0] * (left_zero-1)
+        else:
+            left_zero_list = []
+        if right_zero:
+            right_zero_list = [0] * (right_zero-1)
+        else:
+            right_zero_list = []
+
+        left_zero_list.extend(seats)
+        seats = left_zero_list
+        seats.extend(right_zero_list)
+
+        max_count = 0
+        zero_count = 0
+        for seat in seats:
+            if seat == 0:
+                zero_count += 1
+                if zero_count > max_count:
+                    max_count = zero_count
+            else:
+                zero_count = 0
+        
+        return (max_count - 1) // 2 + 1
+
+
+obj = Solution()
+print(obj.maxDistToClosest([1,0,0,0,1,0,1]))
+print(obj.maxDistToClosest([1,0,0,0]))
+print(obj.maxDistToClosest([0,1]))
